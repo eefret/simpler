da703e9b08ab9e9758a17c829c24ebea77ce6609
Christopher Herrera <christopher.h@futurelinkcorporation.com>
2015-04-06 18:07:20 -0400

    Initial Commit

---
+++ simpler/src/main/java/com/kaissersoft/simpler/Simplable.java
@@ -0,0 +1,8 @@
+package com.kaissersoft.simpler;
+
+/**
+ * Created by Christopher T. Herrera (eefret) on 06/04/15.
+ */
+public interface Simplable {
+    public static final Simpler log = Simpler.getLogger();
+}
